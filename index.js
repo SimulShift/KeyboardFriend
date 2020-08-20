de2aadd2f77ce2b1ebf1b34a2d9b992f24b7e87f
AlexBerimbolo <arpenacoding@gmail.com>
2020-08-20 16:45:51 -0700

    added keyboard press support - still has bugs

--- index.js
+++ index.js
@@ -10,10 +10,19 @@ for (var i = 0; i<numKeys; i++) {
     console.log(keyChar);
 
     if (this.classList.contains('beenClicked')) {
-      //this.classList.remove('.beenClicked'); 
       this.setAttribute("class", "key");
     } else {
       this.setAttribute("class", "beenClicked");
+    }
+  });
+  document.addEventListener("keydown", function(event) {
+    let keyChar = event.key;
+    console.log(keyChar + 'uwu');
+    let currentKey = document.querySelector("#" + keyChar);
+    if (currentKey.classList.contains('beenClicked')) {
+      currentKey.setAttribute("class", "key");
+    } else {
+      currentKey.setAttribute("class", "beenClicked");
     }
   });
 }
