7dcb677224c2382c8f7b40ad1ff733533115b739
AlexBerimbolo <arpenacoding@gmail.com>
2020-08-20 15:40:33 -0700

    added button event listeners

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ let mode = 'i3';
 let numKeys = document.querySelectorAll("li").length;
 
 for (var i = 0; i<numKeys; i++) {
-  document.querySelectorAll("li")[i].addEventListener("click", () => {
-    alert('donthurtme');
+  document.querySelectorAll("li")[i].addEventListener("click", function() {
+    let keyChar = this.innerHTML.toLowerCase();
+    console.log(keyChar);
+
+    if (this.classList.contains('beenClicked')) {
+      //this.classList.remove('.beenClicked'); 
+      this.setAttribute("class", "key");
+    } else {
+      this.setAttribute("class", "beenClicked");
+    }
   });
 }
